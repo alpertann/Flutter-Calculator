2351724da034ff785b916851691be8488da285a5
alpertann <squelchh8@gmail.com>
2022-12-18 16:21:33 +0300

    Calculator app added.

---
+++ flutter_calculator/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hesap_makinesi/calculator_screen.dart';
+ 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
+
